3ecefc1b970973551025e363273d268958650985
Jeffrey Leibowitz <jleibowitz@bwh.harvard.edu>
2018-06-12 11:48:02 -0400

    tried the DEG analysis on different cell types and found many DEGs, the problem might just be the variability in the cell population

--- edgeR.R
+++ edgeR.R
@@ -1,42 +1,46 @@
-data<-read.xlsx("Monocytes in EAE_fem_apoefloxed.xlsx", rowNames=TRUE, sheet=3)
-rownames(data)<-data[,1]
-Match<-data[,1:2]
-Match[,1]<-NULL
+setwd("/Users/jleibowitz/Desktop/Github/DEG-Analysis/Raw counts")
+data<- read.csv(file = "mergeddata.csv", header = TRUE, sep = ",", quote= "'", dec = ".")
+rownames(data)<-data[,2]
+my_genes_ann3 <- read.delim("my_genes_ann2.csv", header = TRUE, sep = ",", quote= "'", dec = ".")
+Match<-data.frame(my_genes_ann3[,3],my_genes_ann3[,4])
 data[,1:2]<-NULL
+sampleID <- read.delim("Metadata.csv", header = TRUE, sep = ",", quote= "'", dec = ".")
 
 source("http://www.bioconductor.org/biocLite.R")
 biocLite("edgeR")
 
 library(edgeR)
-data[,8:15]<-NULL
-group<-c(rep("AWT",4),rep("BFlox",3))
+group<-sampleID$cell_type##group<-c(rep("AWT",4),rep("BFlox",3))
 group<-factor(group)
 cds<-DGEList(data,group=group)
-cds$genes<-data.frame(Symbol=Match[,1])
-keep<-filterByExpr(cds)
+cds$genes<-data.frame(Symbol=Match[,2])
+cpm(10, mean(cds$samples$lib.size))
+keep <- rowSums(cpm(cds) > 0.81) >= 4 ##keep<-filterByExpr(cds)
 summary(keep)
-cds<-cds[keep, , keep.lib.sizes=FALSE]
+cds<-cds[keep, ]
 cds<-calcNormFactors(cds)
 cds$samples
 plotMD(cpm(cds,log=TRUE),column=1)
 abline(h=0, col="red", lty=2, lwd=2)
 
-points <- c(rep(0,4),rep(1,3))
-colors <- c(rep("black",4),rep("red",3))
+points <- c(0,0,0,0,1,1,1,1)
+colors <- c("black","black","black","black","red","red","red","red")
 plotMDS(cds, col=colors, pch=points)
 legend("topleft", legend=((group)), pch=points, col=colors)
 
 design <- model.matrix(~0+group, data=cds$samples)
 colnames(design)<-levels(cds$samples$group)
+colnames(design)<-c("Mo_R","Mo_sp")
 cds<-estimateDisp(cds,design,robust=TRUE)
 fit<-glmQLFit(cds,design,robust=TRUE)
-con<-makeContrasts(BFlox-AWT, levels=design)
+con<-makeContrasts(Mo_Sp-Mo_R, levels=design)
 qlf<-glmQLFTest(fit,contrast=con)
 
 #####################trying to figure out why I'm getting so few sig genes
-ex<-exactTest(cds,pair=c("AWT","BFlox"))
-topTags(ex)
+ex<-exactTest(cds,pair=c("Mo_R","Mo_Sp"))
+t<-topTags(ex,n=1482)
 summary(decideTests(ex))
+
 
 
 
